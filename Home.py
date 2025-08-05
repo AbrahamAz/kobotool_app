f0843f8211b1f4477b96516dd0be0098e6bcd109
Abraham Azar <xv421@drc.ngo>
2025-08-05 13:23:26 +0200

    original draft

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config("Home")
+st.title("KoboTools APP")
+
+st.markdown("THIS APP IS STILL UNDER CONSTRUCTION..")
---
+++ pages/1_XLS_to_Label.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("XLS to Label Converter")
+
+st.markdown("THIS APP IS STILL UNDER CONSTRUCTION..")
