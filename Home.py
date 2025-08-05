e574c82922d32cd24381ca78c337841e703cc4f9
Abraham Azar <xv421@drc.ngo>
2025-08-05 16:34:06 +0200

    add a Home page

--- Home.py
+++ Home.py
@@ -1,8 +1,33 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.set_page_config("Home")
-st.title("KoboTools APP")
+st.set_page_config(page_title="DRC KoboTool Utility App",
+                   page_icon="🛠️",
+                   layout = "centered")
 
-st.markdown("THIS APP IS STILL UNDER CONSTRUCTION..")
+st.title("🛠️ KoboToolBox Utility Hub")
+st.markdown("Welcome to the **KoboToolbox Utility Hub** for DRC!")
+
+st.markdown("""
+            This internal tool is designed to help streamline your Kobo workflows by providing easy-to-use interfaces for common admin tasks.
+
+            ### 🔧 Available Tools
+            Below is the list of tools you can use - more will be added soon!
+
+            - ✅ **[Bulk Asset Transfer](./Bulk_Asset_Transfer)**  
+            Transfer multiple Kobo assets from one account to another with just a few clicks.
+
+            ---
+
+            ### 🚀 Coming Soon
+            - Bulk Persmissions Manager
+            - XLS to Label Switcher
+            - Kobo Media Downloader
+            - And many more
+
+            ---
+
+            ### 📬 Feedback?
+            Have ideas or feedback? Get in touch with the Kobo Core Team!
+            """)
+
+st.link_button("Send an Email", url="mailto:kobo.server@drc.ngo")
